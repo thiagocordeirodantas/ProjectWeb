45e3d51bf31fb13d15eabe89e751fa7cfbbd94fb
Thiago Cordeiro <thiagoccordeirodantaszv@gmail.com>
2023-05-27 21:21:25 -0300

    criando itens expansivos mais ou menos infinitos com arrays

--- books/src/App.js
+++ books/src/App.js
@@ -1,16 +1,30 @@
-import '../src/style/App.css';
-import '../src/style/reset.css'
+import './style/App.css';
 import Logo from './Components/Logo';
+import perfil from './img/perfil.svg';
+import sacola from './img/sacola.svg';
 
+const OpcoesDeTexto = ['CATEGORIAS','FAVORITOS','MINHA ESTANTE']
+const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo/>
-      </header> 
+      <header className='App-header'>
+        <Logo></Logo>
+        <ul className='opcoes'>
+            {OpcoesDeTexto.map((texto) => (
+              <li className='opcao'><p>{texto}</p></li>
+            ))}
+          </ul>
+
+          <ul className='icones'>
+              {icones.map((icone) => (
+               <li className='icone'><img src={icone}></img></li>
+              )) }
+          </ul>
+      </header>
     </div>
   );
 }
 
-export default App;
+export default App
